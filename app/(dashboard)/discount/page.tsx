2ac94ab730f75f034424d0a7398707fcbc98e868
Nguyen Minh Thuan <96782417+nmthuann@users.noreply.github.com>
2023-11-08 02:40:56 +0700

    commit_good_night

--- app/(dashboard)/discount/page.tsx
+++ app/(dashboard)/discount/page.tsx
@@ -5,20 +5,20 @@ import { Discount } from "@/types/discount.interface";
 import { GetDiscounts } from "@/actions/discount/get-discounts";
 
 const DiscountsPage = async () => {
-    // const discounts = await GetDiscounts();
-    // const formattedDiscounts: DiscountColumn[] = discounts.map(
-    //     (item: Discount) => ({
-    //         discount_id: String(item.discount_id),
-    //         description: item.description,
-    //         expired: item.expired,
-    //         percent: item.percent,
-    //     })
-    // );
+    const discounts = await GetDiscounts();
+    const formattedDiscounts: DiscountColumn[] = discounts.map(
+        (item: Discount) => ({
+            discount_id: String(item.discount_id),
+            description: item.description,
+            expired: item.expired,
+            percent: item.percent,
+        })
+    );
 
     return (
         <div className="flex-col">
             <div className="flex-1 space-y-4 p-8 pt-6">
-                {/* <DiscountsClient data={formattedDiscounts} /> */}
+                <DiscountsClient data={formattedDiscounts} />
             </div>
         </div>
     );
