159174f3aa01c18a38e9d62e3556e4c31e94a7b0
Nguyen Minh Thuan <96782417+nmthuann@users.noreply.github.com>
2023-11-08 01:33:36 +0700

    commit_update

--- app/(dashboard)/discount/page.tsx
+++ app/(dashboard)/discount/page.tsx
@@ -1,67 +1,67 @@
-// import { DiscountColumn } from "./components/columns";
-// import { DiscountsClient } from "./components/client";
-
-// import { Discount } from "@/types/discount.interface";
-// import { GetDiscounts } from "@/actions/discount/get-discounts";
-
-// const DiscountsPage = async () => {
-//     const discounts = await GetDiscounts();
-//     const formattedDiscounts: DiscountColumn[] = discounts.map(
-//         (item: Discount) => ({
-//             discount_id: String(item.discount_id),
-//             description: item.description,
-//             expired: item.expired,
-//             percent: item.percent,
-//         })
-//     );
-
-//     return (
-//         <div className="flex-col">
-//             <div className="flex-1 space-y-4 p-8 pt-6">
-//                 <DiscountsClient data={formattedDiscounts} />
-//             </div>
-//         </div>
-//     );
-// };
-
-// export default DiscountsPage;
-
-import { FC } from "react";
-import { GetServerSideProps } from "next";
 import { DiscountColumn } from "./components/columns";
 import { DiscountsClient } from "./components/client";
 
 import { Discount } from "@/types/discount.interface";
 import { GetDiscounts } from "@/actions/discount/get-discounts";
 
-interface DiscountsPageProps {
-    formattedDiscounts: DiscountColumn[];
-}
-
-const DiscountsPage: FC<DiscountsPageProps> = ({ formattedDiscounts }) => (
-    <div className="flex-col">
-        <div className="flex-1 space-y-4 p-8 pt-6">
-            <DiscountsClient data={formattedDiscounts} />
-        </div>
-    </div>
-);
-
-export const getServerSideProps: GetServerSideProps<
-    DiscountsPageProps
-> = async () => {
+const DiscountsPage = async () => {
     const discounts = await GetDiscounts();
-    const formattedDiscounts = discounts.map((item: Discount) => ({
-        discount_id: String(item.discount_id),
-        description: item.description,
-        expired: item.expired,
-        percent: item.percent,
-    }));
+    const formattedDiscounts: DiscountColumn[] = discounts.map(
+        (item: Discount) => ({
+            discount_id: String(item.discount_id),
+            description: item.description,
+            expired: item.expired,
+            percent: item.percent,
+        })
+    );
 
-    return {
-        props: {
-            formattedDiscounts,
-        },
-    };
+    return (
+        <div className="flex-col">
+            <div className="flex-1 space-y-4 p-8 pt-6">
+                <DiscountsClient data={formattedDiscounts} />
+            </div>
+        </div>
+    );
 };
 
 export default DiscountsPage;
+
+// import { FC } from "react";
+// import { GetServerSideProps } from "next";
+// import { DiscountColumn } from "./components/columns";
+// import { DiscountsClient } from "./components/client";
+
+// import { Discount } from "@/types/discount.interface";
+// import { GetDiscounts } from "@/actions/discount/get-discounts";
+
+// interface DiscountsPageProps {
+//     formattedDiscounts: DiscountColumn[];
+// }
+
+// const DiscountsPage: FC<DiscountsPageProps> = ({ formattedDiscounts }) => (
+//     <div className="flex-col">
+//         <div className="flex-1 space-y-4 p-8 pt-6">
+//             <DiscountsClient data={formattedDiscounts} />
+//         </div>
+//     </div>
+// );
+
+// export const getServerSideProps: GetServerSideProps<
+//     DiscountsPageProps
+// > = async () => {
+//     const discounts = await GetDiscounts();
+//     const formattedDiscounts = discounts.map((item: Discount) => ({
+//         discount_id: String(item.discount_id),
+//         description: item.description,
+//         expired: item.expired,
+//         percent: item.percent,
+//     }));
+
+//     return {
+//         props: {
+//             formattedDiscounts,
+//         },
+//     };
+// };
+
+// export default DiscountsPage;
